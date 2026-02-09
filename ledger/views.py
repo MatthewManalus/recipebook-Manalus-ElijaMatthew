bd0707a0e66d3cd83b90968e7ece4080fd5ade8d
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-02-09 21:24:16 +0800

    Lab 1: recipebook templates

---
+++ ledger/views.py
@@ -0,0 +1,45 @@
+
+from django.shortcuts import render
+
+def recipes_list(request):
+    ctx = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "link": "/recipe/1",
+            },
+            {
+                "name": "Recipe 2",
+                "link": "/recipe/2",
+            },
+        ]
+    }
+    return render(request, "ledger/recipes_list.html", ctx)
+
+def recipe_1(request):
+    ctx = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {"name": "tomato", "quantity": "3pcs"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "pork", "quantity": "1kg"},
+            {"name": "water", "quantity": "1L"},
+            {"name": "sinigang mix", "quantity": "1 packet"},
+        ],
+    }
+    return render(request, "ledger/recipe_detail.html", ctx)
+
+def recipe_2(request):
+    ctx = {
+        "name": "Recipe 2",
+        "ingredients": [
+            {"name": "garlic", "quantity": "1 head"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "vinegar", "quantity": "1/2cup"},
+            {"name": "water", "quantity": "1 cup"},
+            {"name": "salt", "quantity": "1 tablespoon"},
+            {"name": "whole black peppers", "quantity": "1 tablespoon"},
+            {"name": "pork", "quantity": "1 kilo"},
+        ],
+    }
+    return render(request, "ledger/recipe_detail.html", ctx)
