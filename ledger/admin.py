ab81276d54e97509d87658cd910ae127da20e692
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-02-19 10:04:34 +0800

    Set up the admin page so recipes can manage their ingredients in one place

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [RecipeIngredientInline]
