b4aa6b1ddafc58c9761d81fe9c85b9b58b6cbef2
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-02 11:51:15 +0800

    Implement custom login page and complete password reset flow with proper template structure and URL configuration

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -110,3 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+LOGIN_REDIRECT_URL = "/recipes/list"
+
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+
+LOGIN_URL = "/accounts/login/"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
