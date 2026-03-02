01c9e3a649194ac2caf3b0ffe05d75f20e1993e5
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-02 11:18:56 +0800

    Extend Recipe with author and timestamps for creation and updates

--- ledger/models.py
+++ ledger/models.py
@@ -17,12 +17,22 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="recipes",
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self) -> str:
         return self.name
 
     def get_absolute_url(self) -> str:
         return reverse("ledger:recipe-detail", kwargs={"pk": self.pk})
-
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=255)
