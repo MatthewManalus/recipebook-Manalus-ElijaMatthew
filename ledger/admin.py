7c9b6bf907f97894d121cbd4da39a48b445e85c7
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-12 11:04:57 +0800

    Fix the authentication templates and register Profile properly in the admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+
+from .models import Ingredient, Profile, Recipe, RecipeImage, RecipeIngredient
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+    extra = 0
+
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
     extra = 1
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    inlines = [RecipeIngredientInline]
+    list_display = ("name", "author", "created_on", "updated_on")
+    inlines = [RecipeIngredientInline, RecipeImageInline]
+
+
+admin.site.register(Ingredient)
+admin.site.register(Profile)
