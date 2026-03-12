80c89d6ab8356e94d8b0e17f87872ba948fef4c0
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-12 11:16:38 +0800

    Add logged-in views for creating recipes and uploading recipe images

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import RecipeForm, RecipeImageForm
 from .models import Ingredient, Recipe
 
 
@@ -12,7 +13,10 @@ def recipes_list(request):
 @login_required
 def recipe_detail(request, pk: int):
     recipe = get_object_or_404(
-        Recipe.objects.select_related("author").prefetch_related("ingredients__ingredient"),
+        Recipe.objects.select_related("author").prefetch_related(
+            "ingredients__ingredient",
+            "images",
+        ),
         pk=pk,
     )
     return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
@@ -21,3 +25,43 @@ def recipe_detail(request, pk: int):
 def ingredient_detail(request, pk: int):
     ingredient = get_object_or_404(Ingredient, pk=pk)
     return render(request, "ledger/ingredient_detail.html", {"ingredient": ingredient})
+
+
+@login_required
+def add_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            if hasattr(request.user, "profile"):
+                recipe.author = request.user.profile
+            recipe.save()
+            return redirect(recipe.get_absolute_url())
+    else:
+        form = RecipeForm()
+
+    return render(request, "ledger/recipe_form.html", {"form": form})
+
+
+@login_required
+def add_recipe_image(request, pk: int):
+    recipe = get_object_or_404(Recipe, pk=pk)
+
+    if request.method == "POST":
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe_image = form.save(commit=False)
+            recipe_image.recipe = recipe
+            recipe_image.save()
+            return redirect(recipe.get_absolute_url())
+    else:
+        form = RecipeImageForm()
+
+    return render(
+        request,
+        "ledger/recipeimage_form.html",
+        {
+            "form": form,
+            "recipe": recipe,
+        },
+    )
