cc80b231ec06e5e1c2f40f095cf66d8b6dc4cf22
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-12 11:11:23 +0800

    Configure media files so uploaded recipe images display during development

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -118,3 +118,7 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_URL = "/accounts/login/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
